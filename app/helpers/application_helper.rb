0a5ba872cfe27a874dad41c194357a2d8a0eb9ae
Mikhail Pasichniuk <mikhail.pasichniuk@debtbook.com>
2023-01-11 09:44:00 -0500

    urok 7 time 6-30

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 module ApplicationHelper
+  def nav_tab(title, url, options={})
+    current_page = options.delete :current_page
+
+    css_class = current_page == title ? 'text-white' : 'text-secondary'
+
+    options[:class] = if options[:class]
+                        options[:class] + ' ' + css_class
+                      else
+                        css_class
+                      end
+
+    link_to title, url, options
+  end
+  def currently_at(current_page = '')
+    render partial: 'shared/menu', locals: {current_page: current_page}
+  end
   def full_title(page_title = '')
     base_title = 'AskIt'
 
