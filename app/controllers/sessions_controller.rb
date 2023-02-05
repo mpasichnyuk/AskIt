9d74401b86c9c2039af09d753716cbce98ab7c40
Mikhail Pasichniuk <mikhail.pasichniuk@debtbook.com>
2023-02-04 19:34:13 -0500

    urok 9 time 25-00

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,27 @@
 class SessionsController < ApplicationController
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, only: :destroy
   def new
 
   end
 
   def create
-    render plain: params.to_yaml
+    user = User.find_by email: params[:email]
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back, #{current_user.name_or_email} !"
+      redirect_to root_path
+
+    else
+      flash[:warning] = 'Incorrect email and/or password!'
+      redirect_to new_session_path
+    end
   end
 
   def destroy
+    sign_out
+    flash[:success]= "See you later!"
+    redirect_to root_path
 
   end
 end
