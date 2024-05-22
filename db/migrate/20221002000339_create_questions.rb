2e122f3fe44bc12e8cb49e50e7ac5a84deeadc5c
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-05-22 11:19:36 -0400

    migration to make Question belong to User

--- db/migrate/20221002000339_create_questions.rb
+++ db/migrate/20221002000339_create_questions.rb
@@ -5,6 +5,7 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
     create_table :questions do |t|
       t.string :title
       t.text :body
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
