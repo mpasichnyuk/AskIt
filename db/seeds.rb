cd7296b5ac2bce6a31a8a064a610a8a2778f2903
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-05-22 11:20:09 -0400

    seeds change to create users and then create questions for them using Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#
-#
-30.times do
-  title = Faker::Hipster.sentence(word_count: 3)
-  body = Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4)
-  Question.create title: title, body: body
+
+# create 5 users
+debugger
+5.times do
+  first_name = Faker::Name.first_name
+  puts first_name
+  last_name = Faker::Name.last_name
+  puts last_name
+  full_name = first_name + ' ' + last_name
+  puts full_name
+  user = User.create(name: full_name, email: "#{first_name}.#{last_name}@mail.com", password: 'test')
+  puts user.save
+  puts user
 end
+
+users = User.all
+
+# CREATE 30 RANDOM QUESTIONS AND SAVE TO DB
+30.times do
+  title = Faker::Lorem.sentence(word_count: 3)
+  body = Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4)
+  user = users.sample
+  Question.create title: title, body: body, user: user
+end
+
+puts "seeded database with #{User.count} users and #{Question.count} questions"
