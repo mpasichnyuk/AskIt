8fa8c26d96b7f96893e0dccc240b1833c55747fa
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-05-22 11:23:48 -0400

    add js dependency needed for bootstrap dropdowns to work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import '@popperjs/core'
-import 'bootstrap/js/dist/dropdown'
+// import '@popperjs/core'
+// import 'bootstrap/js/dist/dropdown'
 
 Rails.start()
 Turbolinks.start()
