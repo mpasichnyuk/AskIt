fbd5f4bf6bce975bc3527fe4036724c1478228b7
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-06-13 17:41:13 -0400

    run migrations after updating to rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_04_001019) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_13_213713) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.integer "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -24,8 +26,8 @@ ActiveRecord::Schema.define(version: 2023_04_04_001019) do
     t.string "title", null: false
     t.text "body"
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
@@ -33,8 +35,8 @@ ActiveRecord::Schema.define(version: 2023_04_04_001019) do
     t.string "email", null: false
     t.string "name"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "remember_token_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
