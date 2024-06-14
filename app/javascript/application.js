ec1ca8db028f33c300a6404e6fe9131cf62c8de6
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-06-13 23:16:37 -0400

    this hopefully fixes bootstrap dropdowns

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,14 +3,23 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require popper
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require_tree .
 
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
+import "bootstrap"
 import * as ActiveStorage from "@rails/activestorage";
 import "./channels";
 
-// import '@popperjs/core'
 // import 'bootstrap/js/dist/dropdown'
+// import '@popperjs/core'
 
 Rails.start();
 Turbolinks.start();
