4aa5a40af3cf6de8ebf8d56aed8a29d0fe83211c
Mikhail Pasichnyuk <m.pasichnyuk@gmail.com>
2024-06-13 23:42:19 -0400

    add hotwired turbo rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,6 +14,7 @@
 
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
+import "@hotwired/turbo-rails"
 import "bootstrap"
 import * as ActiveStorage from "@rails/activestorage";
 import "./channels";
